ae8ae5d46b241a9f9ca41855c87a8481140d71ab
Waghmare Sangharatn <wsangham64@gmail.com>
2022-05-06 00:32:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("this is the script fomr the gt learning");
+console.log("this is to be modified ");
+console.log("this is to be modified ");
+console.log("this is to be modified ");
+console.log("this is to be modified ");
