aa1de222f66f52c2e391af8379448169ad3acff7
Waghmare Sangharatn <wsangham64@gmail.com>
2022-05-06 00:39:52 +0530

    second commit

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ console.log("this is to be modified ");
 console.log("this is to be modified ");
 console.log("this is to be modified ");
 console.log("this is to be modified ");
+console.log("this is added to test the code if it is updated or not")
