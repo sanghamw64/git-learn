c2291ca2d4f46127d1e724b5c4469505fbf2fd55
Waghmare Sangharatn <wsangham64@gmail.com>
2022-05-06 00:43:24 +0530

    changed to main

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ console.log("this is to be modified ");
 console.log("this is to be modified ");
 console.log("this is to be modified ");
 console.log("this is to be modified ");
-console.log("this is added to test the code if it is updated or not")
+console.log("this is added to test the code if it is updated or not");
+console.log("changed to main");
